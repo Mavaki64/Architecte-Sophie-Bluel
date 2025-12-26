6d2ea08c26925d33d359b8694340533ad7b5b69f
Killian <killiangayez@gmail.com>
2025-12-26 20:45:56 +0100

    Ajout de la fonction pour récuperer les catégories sur l'api + Ajout de la fonction pour afficher les catégories sur la page index.html + Suppression des catégories insérer dans le code HTML.

---
+++ FrontEnd/js/filters.js
@@ -0,0 +1,37 @@
+/**
+ * Récupère les catégories depuis l'API
+ * @param {string} apiBaseUrl - L'URL de l'API
+ * @returns {Promise<Array<Object>>} Un tableau d'objets représentant les catégories
+ */
+export async function getCategories(apiBaseUrl) {
+    if (!apiBaseUrl){
+        throw new Error(`Aucune URL n'a été fournie pour fetch les données.`);
+    }
+    const response = await fetch(`${apiBaseUrl}categories`);
+    if (!response.ok) {
+        throw new Error(`HTTP ${response.status}`);
+    }
+    const categories = await response.json();
+    return categories;
+}
+
+export function displayCategories(categories) {
+    const filtersContainer = document.querySelector(".filter");
+    filtersContainer.innerHTML = "";
+    for (let i = 0; i < categories.length; i++) {
+        const filter = categories[i];
+        const filterElement = document.createElement("a");
+        filterElement.href = "#";
+        filterElement.innerText = filter.name;
+        filterElement.classList = "filter-item";
+        filterElement.dataset.filterId = filter.id;
+        filtersContainer.append(filterElement);
+    }
+
+    const filterElement = document.createElement("a");
+    filterElement.href = "#";
+    filterElement.innerText = "Tous";
+    filterElement.classList = "filter-item active";
+    filterElement.dataset.filterId = 0;
+    filtersContainer.prepend(filterElement);
+}
--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,8 +1,12 @@
-import {getProjects, displayProjects} from "./works.js";
+import { getProjects, displayProjects } from "./works.js";
+import { displayCategories, getCategories } from "./filters.js";
 
 const apiBaseUrl = `http://localhost:5678/api/`;
 
 document.addEventListener('DOMContentLoaded', async () => {
     const projects = await getProjects(apiBaseUrl);
+    const categories = await getCategories(apiBaseUrl);
+
     displayProjects(projects);
+    displayCategories(categories)
 });
