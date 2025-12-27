aa8108c2a7072f4fa763d3df0ac363bd46702ce4
Killian <killiangayez@gmail.com>
2025-12-27 16:38:02 +0100

    ajout du flux d'authentification (login/logout)

---
+++ FrontEnd/js/login.js
@@ -0,0 +1,47 @@
+import { loginUser, saveAuth, toggleLoginButton } from "./auth.js";
+
+const submitBtn = document.querySelector(`input[type="submit"]`)
+const apiBaseUrl = `http://localhost:5678/api/`;
+const emailElement = document.querySelector("#email");
+const passwordElement = document.querySelector("#password");
+const errorBox = document.querySelector(".error-box")
+
+/**
+ * Vérifie si l'utilisateur est déjà connecté et redirige vers la page d'accueil si c'est le cas
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    if(localStorage.getItem("userId") != null && window.location.href.includes("/login.html")){
+        window.location = "./index.html"
+    }
+});
+
+/**
+ * Gère le clic sur le bouton de connexion et vérifie les données saisies
+ * @param {Event} event - Événement de clic
+ */
+submitBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const emailRegex = /^[A-Za-z0-9_%+-]+(?:\.[A-Za-z0-9_%+-]+)*@(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)+[A-Za-z]{2,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
+    errorBox.innerHTML = "";
+    try{
+        if(!emailRegex.test(emailElement.value)){
+            emailElement.focus();
+            throw new Error("Veuillez saisir une adresse e-mail valide.")
+        }
+        if(!passwordRegex.test(passwordElement.value)){
+            passwordElement.focus();
+            throw new Error("Veuillez saisir un mot de passe valide.")
+        }
+    } catch(error){
+        errorBox.innerHTML = `<p>${error.message}</p>`;
+        return;
+    }
+
+    const loginData = await loginUser(apiBaseUrl, emailElement.value, passwordElement.value, errorBox);
+    if(loginData.userId != null && loginData.token != null && loginData.userId != undefined && loginData.token != undefined){
+        saveAuth(loginData.userId, loginData.token);
+        toggleLoginButton(document.querySelector(".login"));
+        window.location = './index.html';
+    }
+});
--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,5 +1,6 @@
 import { getProjects, displayProjects } from "./works.js";
 import { displayCategories, getCategories, filterByCategory } from "./filters.js";
+import { logout, toggleLoginButton } from "./auth.js";
 
 const apiBaseUrl = `http://localhost:5678/api/`;
 const filterContainer = document.querySelector(".filter");
@@ -7,13 +8,21 @@ const loginBtn = document.querySelector(".login");
 let projects;
 let categories;
 
+/**
+ * Charge les projets et les catégories et affiche les projets et les catégories
+ */
 document.addEventListener('DOMContentLoaded', async () => {
     projects = await getProjects(apiBaseUrl);
     categories = await getCategories(apiBaseUrl);
     displayProjects(projects);
     displayCategories(categories);
+    toggleLoginButton(loginBtn);
 });
 
+/**
+ * Gère le clic sur le bouton de filtrage et filtre les projets par catégorie
+ * @param {Event} event - Événement de clic
+ */
 filterContainer.addEventListener('click', (event) => {
     if(event.target.classList.contains("filter-item active")){
         event.preventDefault();
@@ -26,6 +35,17 @@ filterContainer.addEventListener('click', (event) => {
     }
 });
 
+/**
+ * Gère le clic sur le bouton de connexion et redirige vers la page de connexion si l'utilisateur n'est pas connecté
+ * @param {Event} event - Événement de clic
+ */
 loginBtn.addEventListener('click', () => {
-    window.location = "./login.html"
+    if (loginBtn.textContent.includes("login")) {
+        window.location = "./login.html"
+        console.log("login");
+    } else {
+        logout();
+        toggleLoginButton(loginBtn);
+        console.log("logout");
+    }
 });
